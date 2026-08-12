2e50e4a6d71ae7cb2f8607046bd0bb50407c0dcb
justsumit58 <158418036+justsumit58@users.noreply.github.com>
2026-08-12 12:11:58 +0530

    feat: setup express server with health check route

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.get("/health",(req,res)=>{
+    res.send("Website is working properly");
+})
+app.listen(8000,()=>{
+    console.log("Server is running");
+})
+
