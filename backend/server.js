be0e000ccb3dd026d29c8057fdc4046e0019e190
justsumit58 <158418036+justsumit58@users.noreply.github.com>
2026-08-21 22:07:12 +0530

    test:authControllers using Postman

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
-const authRoutes = require("../routes/authRoutes.js")
 const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+const authRoutes = require("./routes/authRoutes.js")
+app.use("/api/auth",authRoutes);
 app.get("/health",(req,res)=>{
     res.send("Website is working properly");
 })
