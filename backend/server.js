078112ce3a30ab344c77f6419f0680b9906d228d
justsumit58 <158418036+justsumit58@users.noreply.github.com>
2026-09-10 23:40:52 +0530

    feat: add job CRUD, auth middleware, protected routes

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,18 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
 const authRoutes = require("./routes/authRoutes.js")
-app.use("/api/auth",authRoutes);
+const jobRoutes = require('./routes/jobRoutes')
+
+app.use("/api/auth", authRoutes)
+app.use('/api/jobs', jobRoutes)
+
 app.get("/health",(req,res)=>{
     res.send("Website is working properly");
 })
+
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
-const jobRoutes = require('./routes/jobRoutes')
-app.use('/api/jobs', jobRoutes)
